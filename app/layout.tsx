78ac73e6b5637a05d10513ff1a971a2664b6e261
youngyounkim <rladuddbs789@gmail.com>
2023-09-13 18:43:35 +0900

    feat: 모달 컴포넌트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google";
 
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal actionLabel="submit" isOpen title="hello" />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
