e9cedabb6f4290d7b3acedd5cab24b7207db1b0e
youngyounkim <rladuddbs789@gmail.com>
2023-09-13 16:18:01 +0900

    chore: 프로젝트 셋업

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nextjs13_airbnb
+nextjs 13버전 스터디를 위한 aribnb clone 코딩
---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import { Nunito } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Aribnb",
+  description: "Aribnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
