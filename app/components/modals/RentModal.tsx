0d0ec55bce8d9b2b16cae5c4f1d5cb567f5b5230
youngyounkim <rladuddbs789@gmail.com>
2023-09-17 21:28:34 +0900

    feat: rentmodal 기본 구현

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -4,35 +4,116 @@ import { signIn } from "next-auth/react";
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import Modal from "./Modal";
 import { useRouter } from "next/navigation";
 import useRentModal from "@/app/hooks/useRentModal";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../input/CategoryInput";
 
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 const RentModal = () => {
   const router = useRouter();
   const { isOpen, onClose } = useRentModal();
 
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
   const {
     register,
     handleSubmit,
+    setValue,
+    watch,
     formState: { errors },
+    reset,
   } = useForm<FieldValues>({
     defaultValues: {
-      email: "",
-      password: "",
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
     },
   });
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return;
+    }
+
+    return "Back";
+  }, [step]);
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describes your place?"
+        subTitle="Pick a category"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category) => setCustomValue("category", category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
   return (
     <Modal
       isOpen={isOpen}
       title="Airbnb your home"
-      actionLabel="Submit"
+      actionLabel={actionLabel}
       onClose={onClose}
       onSubmit={onClose}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      secondaryActionLabel={secondaryActionLabel}
+      body={bodyContent}
     />
   );
 };
